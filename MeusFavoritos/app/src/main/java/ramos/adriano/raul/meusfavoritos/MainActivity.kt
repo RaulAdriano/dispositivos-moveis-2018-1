3c8e204296efc3a54ee6ea181389a429da03b7e9
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-03-12 19:33:50 -0300

    Favoritos
    add lista de favoritos

--- MeusFavoritos/app/src/main/java/ramos/adriano/raul/meusfavoritos/MainActivity.kt
+++ MeusFavoritos/app/src/main/java/ramos/adriano/raul/meusfavoritos/MainActivity.kt
@@ -2,11 +2,16 @@ package ramos.adriano.raul.meusfavoritos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (favorito in Favoritos.getLista()){
+            lista.append("$favorito \n\n\n")
+        }
     }
 }

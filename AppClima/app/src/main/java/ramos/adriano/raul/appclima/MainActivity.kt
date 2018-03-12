097ad76a4ed3f48aeb9c5c813c58d4c4ca51355c
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-03-12 20:21:30 -0300

    Clima #01
    Projeto e Layout inicial

---
+++ AppClima/app/src/main/java/ramos/adriano/raul/appclima/MainActivity.kt
@@ -0,0 +1,12 @@
+package ramos.adriano.raul.appclima
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

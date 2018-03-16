77f599c2abb6a707424f8093cebec1fe6af39a69
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-03-16 20:48:10 -0300

    App Buscador GitHub
    Layout e Item Menu

---
+++ BuscadorGitHub/app/src/main/java/ramos/adriano/raul/buscadorgithub/MainActivity.kt
@@ -0,0 +1,30 @@
+package ramos.adriano.raul.buscadorgithub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_buscar){
+            Toast.makeText(this,"Clicou",Toast.LENGTH_LONG).show();
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

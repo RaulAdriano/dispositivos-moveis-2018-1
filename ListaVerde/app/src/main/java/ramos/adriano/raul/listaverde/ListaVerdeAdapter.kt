09399acdaa8c89f5f2e84af79dd25817b527228d
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-04-09 20:24:13 -0300

    Lista Verde #01 - RecyclerView
    RecyclerView

---
+++ ListaVerde/app/src/main/java/ramos/adriano/raul/listaverde/ListaVerdeAdapter.kt
@@ -0,0 +1,68 @@
+package ramos.adriano.raul.listaverde
+
+/**
+ * Created by Raul on 09/04/2018.
+ */
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+
+import android.view.View
+
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class ListaVerdeAdapter : RecyclerView.Adapter<ListaVerdeAdapter.NumeroViewHolder> {
+
+    val lista : List<Int>;
+
+    val context : Context;
+
+
+    constructor(context: Context, lista: List<Int>) {
+
+        this.context = context;
+        this.lista = lista;
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumeroViewHolder {
+
+        val numeroListItem = LayoutInflater.from(context).inflate(R.layout.numero_list_item,parent,false);
+        val numeroViewHolder = NumeroViewHolder(numeroListItem);
+        return numeroViewHolder
+
+    }
+
+
+    override fun getItemCount(): Int {
+
+        return lista.size
+
+    }
+
+
+    override fun onBindViewHolder(holder: NumeroViewHolder, position: Int) {
+
+        val numero = lista.get(position)
+        holder.tvItemNumero?.text = numero.toString();
+        val cor =ColorUtils.getViewHolderBackgroundColor(context,position)
+        holder.tvItemNumero?.setBackgroundColor(cor)
+
+    }
+
+
+    inner class NumeroViewHolder : RecyclerView.ViewHolder {
+
+        val tvItemNumero: TextView?
+
+        constructor(itemView: View?) : super(itemView) {
+                tvItemNumero = itemView?.findViewById(R.id.tv_item_numero)
+
+        }
+
+
+    }
+}

42ea499e39bc6f49d1e498b90a8884d86ff653c5
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-04-09 21:02:01 -0300

    Lista Verde #03 - RecyclerView
    OnClick
    OnLongClick

--- ListaVerde/app/src/main/java/ramos/adriano/raul/listaverde/ListaVerdeAdapter.kt
+++ ListaVerde/app/src/main/java/ramos/adriano/raul/listaverde/ListaVerdeAdapter.kt
@@ -18,14 +18,16 @@ class ListaVerdeAdapter : RecyclerView.Adapter<ListaVerdeAdapter.NumeroViewHolde
     val lista : List<Int>;
 
     val context : Context;
+    val itemClickListener: ItemClickListener;
 
     var viewHolderCount = 0;
 
 
-    constructor(context: Context, lista: List<Int>) {
+    constructor(context: Context, lista: List<Int>, itemClickListener: ItemClickListener) {
 
         this.context = context;
         this.lista = lista;
+        this.itemClickListener = itemClickListener
 
 
     }
@@ -57,17 +59,36 @@ class ListaVerdeAdapter : RecyclerView.Adapter<ListaVerdeAdapter.NumeroViewHolde
         val numero = lista.get(position)
         holder.tvItemNumero?.text = numero.toString();
 
-        
+
 
     }
 
 
-    inner class NumeroViewHolder : RecyclerView.ViewHolder {
+    interface ItemClickListener{
+        fun onItemClick(position: Int)
+        fun onLongItemClick(position: Int)
+    }
+
+
+    inner class NumeroViewHolder : RecyclerView.ViewHolder, View.OnLongClickListener, View.OnClickListener {
+        override fun onClick(p0: View?) {
+            val posicaoClicada = adapterPosition
+            itemClickListener.onItemClick(posicaoClicada)
+        }
+
+        override fun onLongClick(p0: View?): Boolean {
+            val posicaoClicada = adapterPosition
+            itemClickListener.onLongItemClick(posicaoClicada)
+            return true
+        }
 
         val tvItemNumero: TextView?
 
         constructor(itemView: View?) : super(itemView) {
                 tvItemNumero = itemView?.findViewById(R.id.tv_item_numero)
+
+                itemView?.setOnClickListener(this);
+                itemView?.setOnLongClickListener(this)
 
         }
 

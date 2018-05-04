2afdd8d10f267ce1a49a758ca8ca1bb094ec66cb
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-05-04 20:28:38 -0300

    App Ciclo de vida
    recuperar dados

--- CicloDeVida/app/src/main/java/ramos/adriano/raul/ciclodevida/MainActivity.kt
+++ CicloDeVida/app/src/main/java/ramos/adriano/raul/ciclodevida/MainActivity.kt
@@ -2,38 +2,62 @@ package ramos.adriano.raul.ciclodevida
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Create","Create")
+
+
+        if (savedInstanceState!=null){
+            if(savedInstanceState.containsKey("ConteudoSalvo")){
+                val conteudoSalvo = savedInstanceState.getString("ConteudoSalvo")
+                tv_mensagem_log.text = conteudoSalvo
+            }
+        }
+        imprimir("Create")
+
     }
 
     override fun onStart() {
         super.onStart()
-        Log.e("Start","Start")
+        imprimir("Start")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.e("Restart","Restart")
+        imprimir("Restart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.e("Resume","Resume")
+        imprimir("Resume")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.e("Stop","Stop")
+        imprimir("Stop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e("Destroy","Destroy")
+        imprimir("Destroy")
     }
+
+    fun imprimir(msg: String){
+        Log.e("texto",msg)
+        tv_mensagem_log.append("\n"+msg)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        val conteudo =  tv_mensagem_log.text
+        outState?.putString("ConteudoSalvo",conteudo.toString())
+    }
+
+
 }
